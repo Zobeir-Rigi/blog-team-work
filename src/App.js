b0806cdf5f5753bb020a0a747aafbbcc4f093257
Zobeir-Rigi <rigi.zobir@yahoo.com>
2023-05-02 11:47:41 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import Home from "./comp/Home";
-// import Write from "./comp/Write";
+import Write from "./comp/Write";
 // import Login from "./comp/Login";
 // import Register from "./comp/Register";
 // import Settings from "./comp/Settings";
 import Single from "./comp/Single";
 import TopBar from "./comp/TopBar";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   // const user = false;
   return (
-    <BrowserRouter>
-      <TopBar />
+    <BrowserRouter> 
+        <TopBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/write" element={user ? <Write /> : <Register />} /> */}
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/register" element={user ? <Home /> : <Register />} /> */}
-        {/* <Route path="/settings" element={user ? <Settings /> : <Register />} /> */}
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
         <Route path="/post/:postId" element={<Single />} />
       </Routes>
     </BrowserRouter>
