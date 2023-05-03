24591992437e81e5a9567579d054575f60f433ce
Zobeir-Rigi <rigi.zobir@yahoo.com>
2023-05-03 13:46:16 +0100

    Project Frontend section done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Home from "./comp/Home";
 import Write from "./comp/Write";
-// import Login from "./comp/Login";
+import Login from "./comp/Login";
 import Register from "./comp/Register";
-// import Settings from "./comp/Settings";
+import Settings from "./comp/Settings";
 import Single from "./comp/Single";
 import TopBar from "./comp/TopBar";
 
@@ -16,9 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/write" element={user ? <Write /> : <Register />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={user ? <Home /> : <Register />} />
-        {/* <Route path="/settings" element={user ? <Settings /> : <Register />} /> */}
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
         <Route path="/post/:postId" element={<Single />} />
       </Routes>
     </BrowserRouter>
---
+++ src/comp/Login.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import "../styles/Login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm">
+        <label>Email</label>
+        <input
+          className="loginInput"
+          type="email"
+          placeholder="Enter you'r email..."
+        />
+        <label>Password</label>
+        <input
+          className="loginInput"
+          type="password"
+          placeholder="Enter you'r password..."
+        />
+        <button className="loginButton">Login</button>
+      </form>
+      <button className="loginRegisterButton">
+        <Link className="link" to="/register">
+          Register
+        </Link>
+      </button>
+    </div>
+  );
+};
+
+export default Login;
